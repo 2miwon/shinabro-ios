02c70482a0452ecb8b0c5defdac1061106b37b69
nemanjabenkovic <evolroop@naver.com>
2024-07-26 15:23:14 -0700

    setting: Build Project

---
+++ shinabro/shinabro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shinabro
+//
+//  Created by 신민규 on 7/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
